532b0ced48daca00d97ee58384ee355975fad0fa
Colin Morgan <colin.morgan@fullbit.ca>
2017-08-28 17:19:48 -0230

    Add weather api lib

---
+++ weather.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def get_weather(city, state, when):
+    api_url = ('http://api.worldweatheronline.com/premium/v1/'
+                'past-weather.ashx')
+
+    resp = requests.get(
+        api_url,
+        params={
+            'format': 'json',
+            'q': city,
+            'state': state,
+            'date': when,
+            'key': '05e97277c4584e308f3165121172808',
+        }
+    )
+    resp.raise_for_status()
+    return resp.json()
