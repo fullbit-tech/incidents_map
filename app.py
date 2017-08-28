23c95dea3e0f64209db00fb59f55bf4cada42414
Colin Morgan <colin.morgan@fullbit.ca>
2017-08-28 17:20:46 -0230

    Add endpoint for listing and adding an incident

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+import json
+from flask import (Flask, render_template, request, jsonify)
+
+from weather import get_weather
+from parcel import get_parcel
+
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/incidents', methods=['GET', 'POST'])
+def incidents():
+    incidents = []
+    if request.method == 'POST' and 'incident-file' in request.files:
+        try:
+            incident = json.loads(request.files['incident-file'].read())
+            incident['weather'] = get_weather(
+                incident['address']['city'],
+                incident['address']['state'],
+                incident['description']['event_opened'],
+            )['data']['weather'][0]
+            incident['parcel'] = get_parcel(
+                incident['address']['address_line1'])
+            incidents.append(incident)
+        except (TypeError, ValueError):
+            return jsonify({'error': 'Unable to parse incident data'})
+    return jsonify(incidents)
